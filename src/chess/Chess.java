bf88941001593342decc7e42ddfb86f30a29d329
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2015-03-17 23:13:15 -0700

    fixed format

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -13,29 +13,29 @@ public class Chess {
   /**
    * Object reference for player 1.
    */
-	private Player player1;
-	
-	/**
-	 * Object reference for player 2.
-	 */
-	private Player player2;
-	
-	/**
-	 * 2 x 2 matrix that represents the chess board for the game.
-	 */
-	private int[][] board;
+  private Player player1;
 
-	/**
-	 * Initializes chess game.
-	 */
-	public Chess() {
+  /**
+   * Object reference for player 2. 
+   */
+  private Player player2;
+
+  /**
+   * 2 x 2 matrix that represents the chess board for the game.
+   */
+  private int[][] board;
+
+  /**
+   * Initializes chess game.
+   */
+  public Chess() {
     player1 = new Player(1);
     player2 = new Player(2);
-		board = new int[8][8];
-	}
+    board = new int[8][8];
+  }
 
-	public static void main(String[] args) {
-		Chess game = new Chess();
-	}
+  public static void main(String[] args) {
+    Chess game = new Chess();
+  }
 
 }
